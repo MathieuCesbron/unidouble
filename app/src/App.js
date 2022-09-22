6cec5080b90e14ab96c3d7521665ef3c46344e3f
Mathieu Cesbron <mathieu.cesbron@protonmail.com>
2022-09-22 21:02:16 +0200

    Implement wallet adapter
    I need to add config-overrides.js otherwise we get errors.

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,51 @@
-import './App.css';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+  LedgerWalletAdapter,
+  SolletWalletAdapter,
+  GlowWalletAdapter,
+  SlopeWalletAdapter,
+} from '@solana/wallet-adapter-wallets'
+import { clusterApiUrl } from '@solana/web3.js'
+import React, { useMemo } from 'react'
+
+require('@solana/wallet-adapter-react-ui/styles.css')
 
 export default function App() {
   return (
-    <div className="App">
-      Start of Unidouble
-    </div>
+    <Context>
+      <div className="App">
+        <WalletMultiButton />
+      </div>
+    </Context>
+  )
+}
+
+const Context = ({ children }) => {
+  const network = WalletAdapterNetwork.Devnet
+  const endpoint = useMemo(() => clusterApiUrl(network), [network])
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new TorusWalletAdapter(),
+      new LedgerWalletAdapter(),
+      new SolletWalletAdapter(),
+      new GlowWalletAdapter(),
+      new SlopeWalletAdapter(),
+    ],
+    []
+  )
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   )
 }
