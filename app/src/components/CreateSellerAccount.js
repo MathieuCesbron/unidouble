7e2cc918bf2d49f52671c113a86b85934c089a45
Mathieu Cesbron <mathieu.cesbron@protonmail.com>
2022-09-29 21:33:32 +0200

    Add text to modal Create seller account

--- app/src/components/CreateSellerAccount.js
+++ app/src/components/CreateSellerAccount.js
@@ -1,41 +1,11 @@
 import React, { useEffect, useState } from "react"
-import { useWallet } from "@solana/wallet-adapter-react"
-import { PublicKey, SystemProgram } from "@solana/web3.js"
 
 import "./CreateSellerAccount.css"
 import CreateSellerAccountLogo from "../images/create-seller-account-logo.png"
-import { program, programID, storePubKey } from "../utils/solana"
-import { curve } from "../utils/crypto"
 import ModalCreateSellerAccount from "../components/modals/ModalCreateSellerAccount"
 
 export default function CreateSellerAccount() {
-    const { publicKey } = useWallet()
     const [showModalCreateSellerAccount, setShowModalCreateSellerAccount] = useState(false)
-
-    // const createSellerAccountOnChain = async () => {
-    //     const sellerDiffieKeyPair = curve.genKeyPair()
-    //     const sellerDiffiePubKey = sellerDiffieKeyPair.getPublic().encode("hex", true)
-    //     const [sellerAccount] = await PublicKey.findProgramAddress(
-    //         [publicKey.toBuffer()],
-    //         programID
-    //     )
-
-    //     try {
-    //         const tx = await program.methods
-    //             .createSellerAccount(sellerDiffiePubKey)
-    //             .accounts(
-    //                 {
-    //                     user: publicKey,
-    //                     sellerAccount: sellerAccount,
-    //                     store: storePubKey,
-    //                     systemProgram: SystemProgram.programID
-    //                 })
-    //             .rpc()
-    //         console.log(tx)
-    //     } catch (error) {
-    //         console.log("error: ", error)
-    //     }
-    // }
 
     return (
         <div className="create-seller-account">
--- app/src/components/modals/ModalCreateSellerAccount.js
+++ app/src/components/modals/ModalCreateSellerAccount.js
@@ -1,8 +1,41 @@
 import React from "react"
+import { useWallet } from "@solana/wallet-adapter-react"
+import { PublicKey, SystemProgram } from "@solana/web3.js"
 
+import { program, programID, storePubKey } from "../../utils/solana"
+import { curve } from "../../utils/crypto"
 import "./Modals.css"
 
+
 export default function ModalCreateSellerAccount({ setShowModalCreateSellerAccount }) {
+    const { publicKey } = useWallet()
+
+    const sellerDiffieKeyPair = curve.genKeyPair()
+    const sellerDiffiePubKey = sellerDiffieKeyPair.getPublic().encode("hex", true)
+
+    const createSellerAccountOnChain = async () => {
+        const [sellerAccount] = await PublicKey.findProgramAddress(
+            [publicKey.toBuffer()],
+            programID
+        )
+
+        try {
+            const tx = await program.methods
+                .createSellerAccount(sellerDiffiePubKey)
+                .accounts(
+                    {
+                        user: publicKey,
+                        sellerAccount: sellerAccount,
+                        store: storePubKey,
+                        systemProgram: SystemProgram.programID
+                    })
+                .rpc()
+            console.log(tx)
+        } catch (error) {
+            console.log("error: ", error)
+        }
+    }
+
     return (
         <div className="modal-background">
             <div className="modal">
@@ -12,7 +45,12 @@ export default function ModalCreateSellerAccount({ setShowModalCreateSellerAccou
                         onClick={() => setShowModalCreateSellerAccount(false)}
                     >EXIT</button>
                 </div>
-                PLACEHOLDER
+                <h3>Create seller account</h3>
+                <hr />
+                <p>This is your private key, save this somewhere safe, we won't show this to you ever again.
+                    It will be needed to decode the sales you make.
+                </p>
+                <h4>{sellerDiffiePubKey}</h4>
             </div>
         </div>
     )
