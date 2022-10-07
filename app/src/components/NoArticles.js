ce8082c169421e0ddab9311ce5b55c9b9072a756
Mathieu Cesbron <mathieu.cesbron@protonmail.com>
2022-10-07 21:34:05 +0200

    Add NoArticles content page

--- app/src/components/NoArticles.js
+++ app/src/components/NoArticles.js
@@ -1,9 +1,19 @@
+import { useNavigate } from "react-router-dom"
 import React from "react"
 
+import backBtn from "../images/back-btn.png"
+import "./NoArticles.css"
+
 export default function NoArticles() {
+    const navigate = useNavigate()
+
     return (
-        <div>
-            PLACEHOLDER
+        <div className="no-articles">
+            <h2>You have no articles listed yet</h2>
+            <img
+                className="no-articles-back-btn"
+                src={backBtn}
+                onClick={() => navigate("/seller-account")} />
         </div>
     )
 }
