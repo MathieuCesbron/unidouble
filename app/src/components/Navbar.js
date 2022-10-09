fcff630f93e6e2d038a3891e2a18bce448cc76d9
Mathieu Cesbron <mathieu.cesbron@protonmail.com>
2022-10-09 18:47:11 +0200

    Add search btn on navbar

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from "react"
 import Select from 'react-select'
-import { useNavigate } from 'react-router-dom';
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { useNavigate } from 'react-router-dom'
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui"
 
 import "./Navbar.css"
 import useStore from "../store"
-import UnidoubleLogo from "../images/unidouble-logo.png"
+import search from "../images/search.png"
+import unidoubleLogo from "../images/unidouble-logo.png"
 import { countries } from "../config/countries"
 import { categories } from "../config/categories"
-import { useWallet } from "@solana/wallet-adapter-react";
+import { useWallet } from "@solana/wallet-adapter-react"
 
 
 export default function Navbar() {
@@ -36,22 +37,25 @@ export default function Navbar() {
 
     return (
         <div className="navbar">
-            <img className="unidouble-logo" src={UnidoubleLogo} onClick={setHomePage} />
+            <img className="unidouble-logo" src={unidoubleLogo} onClick={setHomePage} />
             <>
                 <Select
-                    className="select select-country"
+                    className="select"
                     placeholder="Select country"
                     options={countries}
                     value={countries.find(c => c.value === country)}
                     onChange={setCountry}
                 />
                 <Select
-                    className="select select-category"
+                    className="select"
                     placeholder="Select category"
                     options={categories}
                     value={categories.find(c => c.value === category)}
                     onChange={setCategory}
                 />
+                <button className="navbar-search-btn">
+                    <img className="navbar-search-logo" src={search} />
+                </button>
             </>
             <button disabled={!connected} className="navbar-btn navbar-btn-my-seller-account" onClick={setSellerAccountPage}>My seller account</button>
             <button disabled={!connected} className="navbar-btn" onClick={setMyPurchasesPage}>My purchases</button>
