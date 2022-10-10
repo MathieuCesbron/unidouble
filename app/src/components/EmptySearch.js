dc896dfd941f782c2d7418774e027b51cfbe785c
Mathieu Cesbron <mathieu.cesbron@protonmail.com>
2022-10-10 21:43:18 +0200

    Add EmptySearch result

--- app/src/components/EmptySearch.js
+++ app/src/components/EmptySearch.js
@@ -1,9 +1,20 @@
 import React from "react"
+import { useNavigate } from "react-router-dom"
+
+import backBtn from "../images/back-btn.png"
+import "./EmptySearch.css"
 
 export default function EmptySearch() {
+    const navigate = useNavigate()
+
     return (
-        <div>
-            EMPTY SEARCH
+        <div className="no-articles">
+            <h2>No articles listed yet in this category</h2>
+            <img
+                className="no-articles-back-btn"
+                src={backBtn}
+                onClick={() => navigate("/")}
+            />
         </div>
     )
 }
