3d1826ec2a065b8885a902e1acfe4a48fa712abf
Mathieu Cesbron <mathieu.cesbron@protonmail.com>
2022-10-27 21:19:33 +0200

    Add front page
    The front page needs to be non predatory.

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,9 +1,17 @@
 import React from "react"
+import { useWallet } from "@solana/wallet-adapter-react"
+
+import SearchImage from "../images/search-image.png"
+import ConnectWalletImage from "../images/connect-wallet-image.png"
+import "./Home.css"
 
 export default function Home() {
+    const { connected } = useWallet()
+
     return (
-        <div>
-            PLACEHOLDER
+        <div className="home">
+            <img className="home-search-image" src={SearchImage} />
+            {!connected && <img className="connect-wallet-image" src={ConnectWalletImage} />}
         </div>
     )
 }
